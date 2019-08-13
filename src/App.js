e13fbfa6eb20b14eb905450b7873bdc08ce804f8
Chase Dudas <chasedudas13@gmail.com>
2019-08-13 15:48:08 -0600

    Navbar info and div

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import { Route, NavLink, HashRouter } from "react-router-dom";
-import { Nav, NavDropdown, Navbar } from 'react-bootstrap'
+import { Nav, NavDropdown, Navbar, Menu } from 'react-bootstrap'
+import { Dropdown, DropdownButton } from 'react-bootstrap/DropdownButton'
+
+import './App.css';
 
 import Home from "./components/HomePage";
 
@@ -9,8 +12,8 @@ class App extends Component {
   render() {
     return (
       <HashRouter>
-        <div>
-          <Nav justify className="justify-content-center">
+        <div id = "homeSection">
+          <Nav justify className="justify-content-center" id='navBarTop'>
             <Nav.Item>
               <Nav.Link href="/">Home</Nav.Link>
             </Nav.Item>
@@ -24,21 +27,11 @@ class App extends Component {
               <Nav.Link href="/blog">Blog</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link href="/projects">Projects</Nav.Link>
+              <Nav.Link href="#projects">Projects</Nav.Link>
             </Nav.Item>
           </Nav>
         </div>
 
-
-        <div>
-          <h1>Welcome!</h1>
-          <ul className="header">
-            <li><NavLink to="/home">Home</NavLink></li>
-          </ul>
-          <div className="content">
-            <Route path="/home" component={Home}/>
-          </div>
-        </div>
 
         <div id='aboutSection'>
           <p>
@@ -46,6 +39,17 @@ class App extends Component {
           </p>
         </div>
 
+        <div id='projectSection'>
+
+        </div>
+
+        <div id='contactSection'>
+
+        </div>
+
+        <div id='footerSection'>
+
+        </div>
       </HashRouter>
     );
   }
